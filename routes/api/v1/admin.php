9213869f6439167fb270a127915fff40ae7b5c53
ferdawus <ferdawusa@gmail.com>
2022-08-17 10:47:25 +0600

    fast

---
+++ routes/api/v1/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Admin\AdminAuthController;
+
+
+Route::controller(AdminAuthController::class)->group(function(){
+    Route::post('/login','login');
+    Route::post('/register','register');
+});
---
+++ routes/api/v1/seller.php
@@ -0,0 +1,9 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Seller\SellerAuthController;
+
+
+Route::controller(SellerAuthController::class)->group(function(){
+    Route::post('/login','login');
+    Route::post('/register','register');
+});
